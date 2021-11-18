cba05be0b968907f6b6602daca25911d1e0365a2
zoster2315 <zoster2315@gmail.com>
2021-11-18 11:53:36 +0400

    исправление ссылок после откючения git lfs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject crashParticle;
+
     private void OnParticleCollision(GameObject other)
     {
         //Debug.Log($"{gameObject.name} hited by {other.gameObject.name}");
+        Instantiate(crashParticle, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
